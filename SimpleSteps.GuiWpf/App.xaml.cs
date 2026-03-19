2855ec8839d52e29c6edced4d3375a4aa0c2492e
synthieat <horst.schneider@hotmail.com>
2026-03-19 21:35:25 +0100

    Add project files.

---
+++ SimpleSteps.GuiWpf/App.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace SimpleSteps.GuiWpf
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+
+
+    }
+
+}
